b7080db5cc161ae0c459f1e2a409f030e2db98d7
seoterran <seoterran@gmail.com>
2018-09-09 14:31:36 -0700

    erase credentials

--- config.js
+++ config.js
@@ -9,17 +9,15 @@ const test=false,
         },
         test:true,
         port:3000,
-        apiKey: '46143052',//'45933082', // process.env.API_KEY,
-        apiSecret: '4464230683e5856b0c161dfbbd120fa761dc9324',//'4df52d6baab53c2a93be8b5f18d243a9dc8c2905', // process.env.API_SECRET
-        dbURI: 'mongodb://eksafdas:abc1035@ds121014.mlab.com:21014/heroku_2zt4zt62',//'mongodb://localhost/ConnectionTest';
+        apiKey: '***',// process.env.API_KEY,
+        apiSecret: '***', // process.env.API_SECRET
+        dbURI: '***',//'mongodb://localhost/ConnectionTest';
         mail: {
-            // account: "gmail",
-            // user: "coffeeenglish2@gmail.com",
-            // password: "^&jw6767"
-            tweengle_email:"help@heyletz.com",
-            SENDGRID_APIKEY : "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"
+
+            tweengle_email:"***",
+            SENDGRID_APIKEY : "***"
         },
-        ot_embed_id: '5a5a6c7c-0b08-4c29-8e38-1b00f51aeafe',
+        ot_embed_id: '***',
         host : "localhost:3000"
     },
     prod_version = {
@@ -39,18 +37,16 @@ const test=false,
       },
       test:false,
       port:8080,
-      apiKey: '46143052',//'45933082', // process.env.API_KEY,
-      apiSecret: '4464230683e5856b0c161dfbbd120fa761dc9324',//'4df52d6baab53c2a93be8b5f18d243a9dc8c2905', // process.env.API_SECRET
-      dbURI: 'mongodb://eksafdas:abc1035@ds121014.mlab.com:21014/heroku_2zt4zt62',//'mongodb://localhost/ConnectionTest';
+      apiKey: '***',
+      apiSecret: '***',// process.env.API_SECRET
+      dbURI: '***',//'mongodb://localhost/ConnectionTest';
       mail: {
-          // account: "gmail",
-          // user: "coffeeenglish2@gmail.com",
-          // password: "^&jw6767"
-          tweengle_email:"help@heyletz.com",
-          SENDGRID_APIKEY : "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"
+ 
+          tweengle_email:"***",
+          SENDGRID_APIKEY : "***"
       },
-      ot_embed_id: 'cd2de047-8fa4-41ff-828a-3a377cca89d8',
-      host : "www.heyLetz.com"
+      ot_embed_id: '***',
+      host : "***"
     };
 
 if(test)
